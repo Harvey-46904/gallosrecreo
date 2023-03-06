50bfbb1790f3d0249a028e1e1919552995de1d4c
Harvey-46904 <harveympv@hotmail.com>
2023-03-06 09:56:15 -0500

    interfas dashboard

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('webdashboar.index');
 });
-
 
 Route::resource("ave","AvesController");
